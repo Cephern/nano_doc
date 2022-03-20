cb6a42ed4ab975c16ef16de6d2ec78996585b2e5
JagMax <chronophobia95@gmail.com>
2022-03-20 15:41:16 +0300

    Appointment styling

--- src/pages/Appointment/Appointment.js
+++ src/pages/Appointment/Appointment.js
@@ -16,90 +16,93 @@ import { useDoctorsContext } from "../../hooks/useDoctorsContext";
 
 // option for Select
 const options = [
-  { value: "caries", label: "Лечение Кариеса" },
-  { value: "pulpitis", label: "Лечение Пульпита" },
-  { value: "prof", label: "Профилактический Осмотр" },
+	{ value: "caries", label: "Лечение Кариеса" },
+	{ value: "pulpitis", label: "Лечение Пульпита" },
+	{ value: "prof", label: "Профилактический Осмотр" },
 ];
 
 export default function Appointment() {
-  const { doctors } = useDoctorsContext();
+	const { doctors } = useDoctorsContext();
 
-  const [DoctorsOptions, setDoctorsOptions] = useState(null);
+	const [DoctorsOptions, setDoctorsOptions] = useState(null);
 
-  const [startDate, setStartDate] = useState(new Date());
-  const [selectedReason, setSelectedReason] = useState(null);
-  const [selectedDoctor, setSelectedDoctor] = useState(null);
-  const [phone, setPhone] = useState(undefined);
+	const [startDate, setStartDate] = useState(new Date());
+	const [selectedReason, setSelectedReason] = useState(null);
+	const [selectedDoctor, setSelectedDoctor] = useState(null);
+	const [phone, setPhone] = useState(undefined);
 
-  console.log(selectedDoctor);
-  console.log(selectedReason);
-  console.log(startDate);
+	console.log(selectedDoctor);
+	console.log(selectedReason);
+	console.log(startDate);
 
-  useEffect(() => {
-    if (doctors) {
-      setDoctorsOptions(
-        doctors.map((doctor) => {
-          const DoctorName = `${doctor.name.surname} ${doctor.name.firstName} ${doctor.name.lastName}`;
+	useEffect(() => {
+		if (doctors) {
+			setDoctorsOptions(
+				doctors.map((doctor) => {
+					const DoctorName = `${doctor.name.surname} ${doctor.name.firstName} ${doctor.name.lastName}`;
 
-          return {
-            value: doctor.id,
-            label: DoctorName,
-          };
-        })
-      );
-    }
-  }, [doctors]);
+					return {
+						value: doctor.id,
+						label: DoctorName,
+					};
+				})
+			);
+		}
+	}, [doctors]);
 
-  console.log(DoctorsOptions);
+	console.log(DoctorsOptions);
 
-  return (
-    <div className="Appointment">
-      <Greeting />
+	return (
+		<div className="Appointment">
+			<h2 className="title">Запись на прием</h2>
+			<form>
+				<label>
+					<Select
+						className="select"
+						options={options}
+						placeholder="Выберите Цель Визита"
+						onChange={(reason) => setSelectedReason(reason)}
+						value={selectedReason}
+					/>
+				</label>
 
-      <form>
-        <h2>Запись на Прием</h2>
+				<label>
+					<Select
+						className="select"
+						options={DoctorsOptions}
+						placeholder={
+							DoctorsOptions ? "Выберите врача" : "Собираем консилиум..."
+						}
+						onChange={(doctor) => setSelectedDoctor(doctor)}
+						value={selectedDoctor}
+					/>
+				</label>
 
-        <label>
-          <Select
-            options={options}
-            placeholder="Выберите Цель Визита"
-            onChange={(reason) => setSelectedReason(reason)}
-            value={selectedReason}
-          />
-        </label>
+				<label>
+					<span>Выберите дату приема:</span>
+					<DatePicker
+						className="datepicker"
+						selected={startDate}
+						onChange={(date) => setStartDate(date)}
+					/>
+				</label>
 
-        <label>
-          <Select
-            options={DoctorsOptions}
-            placeholder={
-              DoctorsOptions ? "Выберите врача" : "Собираем консилиум..."
-            }
-            onChange={(doctor) => setSelectedDoctor(doctor)}
-            value={selectedDoctor}
-          />
-        </label>
+				<label>
+					<span>Комментарий для врача:</span>
+					<textarea cols="60" rows="3"></textarea>
+				</label>
 
-        <label>
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-        </label>
+				<label>
+					<span>Контактный номер телефона:</span>
+					<PhoneInput
+						placeholder="Введите номер телефона в формате 1 234 567 89 89"
+						value={phone}
+						onChange={(num) => setPhone(num)}
+					/>
+				</label>
 
-        <label>
-          <span>Комментарий для врача:</span>
-          <textarea cols="30" rows="10"></textarea>
-        </label>
-
-        <label>
-          <span>Контактный номер телефона:</span>
-          <PhoneInput
-            placeholder="Введите номер телефона в формате"
-            value={phone}
-            onChange={(num) => setPhone(num)}
-          />
-        </label>
-      </form>
-    </div>
-  );
+				<button>Записаться</button>
+			</form>
+		</div>
+	);
 }
