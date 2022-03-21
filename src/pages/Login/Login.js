45fda9bf72a50b66248d9c1115d0597659965959
JagMax <gewoonmaks@gmail.com>
2022-03-21 15:19:06 +0300

    Login and Signup template

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,9 +2,21 @@
 import "./Login.css";
 
 export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+	return (
+		<div className="Login">
+			<form>
+				<h2>Авторизация</h2>
+				<label>
+					<input type="text" placeholder="логин" />
+				</label>
+
+				<label>
+					<input type="password" placeholder="пароль" />
+				</label>
+
+				<button>Войти</button>
+				<button>Регистрация</button>
+			</form>
+		</div>
+	);
 }
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,9 +2,27 @@
 import "./Signup.css";
 
 export default function Signup() {
-  return (
-    <div>
-      <h1>Signup</h1>
-    </div>
-  );
+	return (
+		<div className="Signup">
+			<form>
+				<h2>Регистрация</h2>
+				<label>
+					<input type="text" placeholder="логин" />
+				</label>
+				<label>
+					<input type="text" placeholder="email" />
+				</label>
+
+				<label>
+					<input type="password" placeholder="пароль" />
+				</label>
+				<label>
+					<input type="password" placeholder="повторите пароль" />
+				</label>
+
+				<button>Регистрация</button>
+				<button>К Авторизации</button>
+			</form>
+		</div>
+	);
 }
